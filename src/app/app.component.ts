f991008a01e54ef14d6867515338a8358179b836
Sonam Choeda <sonam.choeda@selise.ch>
2022-10-21 16:19:41 +0600

    chatbot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'POCO';
+ showBox: boolean = false;
+
+  ngOnInit() {
+
+
+}
+close(status: any){
+   this.showBox = status;
+}
+
 }
